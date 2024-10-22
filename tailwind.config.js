8f692a09051e7d38fe3b8c399734b84c9ac0c5cd
mlmejo <mlmejo12@gmail.com>
2024-10-22 15:18:56 +0800

    Create welcome and login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "san-marino": {
+                    50: "#f4f7fb",
+                    100: "#e8edf6",
+                    200: "#ccdaeb",
+                    300: "#9fbbda",
+                    400: "#6c98c4",
+                    500: "#497bae",
+                    600: "#3c6a9f",
+                    700: "#2e4f76",
+                    800: "#294463",
+                    900: "#263a54",
+                    950: "#1a2537",
+                },
             },
         },
     },
